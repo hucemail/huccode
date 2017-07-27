49997e7fb09b486cb4ad14da3e48621e8ac98733
huangminxing <huangminxing@ddianle.com>
2017-07-27 20:49:52 +0800

    AngularJS

---
+++ frontend/AngularJS/04.ng-cloak解决闪屏/App_Code/WebService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.Services;
+
+/// <summary>
+/// WebService 的摘要说明
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// 若要允许使用 ASP.NET AJAX 从脚本中调用此 Web 服务，请取消注释以下行。 
+ [System.Web.Script.Services.ScriptService]
+public class WebService : System.Web.Services.WebService
+{
+
+    public WebService()
+    {
+        //如果使用设计的组件，请取消注释以下行 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string HelloWorld()
+    {
+        return "Hello World";
+    }
+    [WebMethod]
+    public void TestJson()
+    {
+        var model = new { name = "name", price = 10 };
+        JavaScriptSerializer serializer = new JavaScriptSerializer();
+        var json = serializer.Serialize(model);
+        System.Threading.Thread.Sleep(5000);
+        //主要是下面的两句 
+        Context.Response.Write(json);
+        Context.Response.End();
+    }
+}
+
