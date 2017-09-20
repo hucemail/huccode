eb424677d3964dcd324ab72b6a54f85507a83162
huangminxing <huangminxing@ddianle.com>
2017-09-20 20:50:56 +0800

    NodeJS

---
+++ frontend/NodeJS/01.NodeJS的介绍/HelloWord.js
@@ -0,0 +1,5 @@
+﻿var http = require("http");
+http.createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html;charset=UTF-8" });
+    res.end("Hello Word\n");
+}).listen(3000,"127.0.0.1");
