5c331989899250e50ac0445b3a45558315c058a6
huangminxing <huangminxing@ddianle.com>
2017-10-17 17:55:50 +0800

    nodejs

--- frontend/NodeJS/06.文件上传/server.js
+++ frontend/NodeJS/06.文件上传/server.js
@@ -4,20 +4,41 @@ var http = require('http'),
     formidable = require("formidable"),
     util = require("util"),
     fs = require("fs"),
-    path=require("path"),
+    path = require("path"),
+    uuidv1 = require('uuid/v1'),
     port = process.env.PORT || 1337;
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain;charset=UTF-8' });
     var pathname = url.parse(req.url).pathname;
     if (pathname == "/upload" && req.method.toLocaleLowerCase() == "post") {
-
+        //文件保存路径
+        var saveDir = __dirname + "/uploads";
+        if (!fs.existsSync(saveDir)) {
+            fs.mkdirSync(saveDir);
+        }
+        //文件上传
         var form = new formidable.IncomingForm();
         form.encoding = "utf-8";
-        form.uploadDir = __dirname + "/uploads";
+        form.uploadDir = saveDir;
         form.parse(req, function (err, fields, files) {
-            res.write('received upload:\n\n');
-            res.end(util.inspect({ fields: fields, files: files }));
+            if (err) {
+                throw err;
+            }
+            //扩展名
+            var extname = path.extname(files.tupian.name);
+            //旧路径
+            var oldpath = files.tupian.path;
+            //新路径
+            var newpath = saveDir + "/" + uuidv1() + extname;
+            fs.rename(oldpath, newpath, function (err) {
+                if (err) {
+                    throw err;
+                }
+                res.write('文件上传成功:\n\n');
+                res.end(util.inspect({ fields: fields, files: files }));
+            });
+
         });
         return;
     }
@@ -25,5 +46,8 @@ http.createServer(function (req, res) {
         res.end("主页");
     }
 }).listen(port);
+
+
+
 
 
